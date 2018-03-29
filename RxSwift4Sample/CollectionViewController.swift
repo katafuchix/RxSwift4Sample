3ad8009331f3e81ec0a383cc7b4c313f1f09b56d
katafuchix <katafuchix@gmail.com>
2018-03-30 02:38:46 +0900

    Collection Sample

--- RxSwift4Sample/CollectionViewController.swift
+++ RxSwift4Sample/CollectionViewController.swift
@@ -61,22 +61,75 @@ class CollectionViewController: UIViewController {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        
+        self.collectionView.register(CollectionViewCell.self,
+                                     forCellWithReuseIdentifier: "Cell")
+        
+        let items = Variable(Array(1...10))
+        items.asObservable().bind(to: self.collectionView.rx.items(cellIdentifier: "Cell", cellType: CollectionViewCell.self)){ (_ , element, cell) in
+            //セルのindex、対応する要素、セルが受け取れます。
+            //数字を保持するだけの簡単なセルに突っ込んでいます。
+            
+            
+            cell.label.text = "\(element)"
+            
+            }.disposed(by: self.rx.disposeBag)
+        
+        //下で設定してるdelegateをセットします。
+        collectionView.rx.setDelegate(self).disposed(by: self.rx.disposeBag)
+        
+        collectionView.rx.itemSelected
+            .subscribe(onNext: { [weak self] indexPath in
+                print(indexPath)
+            }).disposed(by: self.rx.disposeBag)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
+}
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+extension CollectionViewController: UICollectionViewDelegateFlowLayout {
+    //セルの間の余白設定
+    func  collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        return 10.0
     }
-    */
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 10.0
+    }
+    
+    //セルのサイズを指定
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: 100, height: 50)
+    }
+}
 
+//自定义单元格
+fileprivate class CollectionViewCell: UICollectionViewCell {
+    
+    var label:UILabel!
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        //背景设为橙色
+        self.backgroundColor = UIColor.orange
+        
+        //创建文本标签
+        label = UILabel(frame: frame)
+        label.textColor = UIColor.white
+        label.textAlignment = .center
+        self.contentView.addSubview(label)
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        label.frame = bounds
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
 }
